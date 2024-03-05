d038d5ec5845ff337b02bc830bd32fd700930197
Dan Newingham <dannewingham@gmail.com>
2024-03-05 16:36:02 +0000

    refactor(homes-app): Add Housing Service to details component

--- homes-app/src/app/details/details.component.ts
+++ homes-app/src/app/details/details.component.ts
@@ -1,23 +1,28 @@
 import { Component, inject } from '@angular/core';  //inject gives access to the url parameter
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housing-location';
 
+//Typescript note: '?' is the optional chaining operator from TS and guards against calling .id on undefined
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [CommonModule],
   template: `
     <p>
-      details works! {{ housingLocationId }}
+      details works! {{ housingLocation?.id }}  
     </p>
   `,
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
-  housingLocationId = 0;
+  housingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
 
   constructor() {
-    this.housingLocationId = Number(this.route.snapshot.params["id"]);
+    const housingLocationId = Number(this.route.snapshot.params["id"]);
+    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
   }
 }
