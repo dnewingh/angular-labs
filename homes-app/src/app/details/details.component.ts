3f6a1c3146dfa8b723394799967aec34855668b7
Dan Newingham <dannewingham@gmail.com>
2024-03-06 10:44:20 +0000

    feat(homes-app): Add form data model and control elements
    https://www.youtube.com/watch?v=kWbk-dOJaNQ&t=3s

--- homes-app/src/app/details/details.component.ts
+++ homes-app/src/app/details/details.component.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housing-location';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 //Typescript note: '?' is the optional chaining operator from TS and guards against calling .id on undefined
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
     <article>
       <img class="listing-photo" [src]="housingLocation?.photo">
@@ -26,7 +27,17 @@ import { HousingLocation } from '../housing-location';
       </section>
       <section class="listing-apply">
         <h2 class="section-heading">Apply to live here</h2>
-        <button class="primary" type="button">Apply now</button>
+        <form [formGroup]="applyForm">
+          <label for="first-name">First Name</label>
+          <input id="first-name" type="text" formControlName="firstName">
+
+          <label for="last-name">First Name</label>
+          <input id="last-name" type="text" formControlName="lastName">
+
+          <label for="email">First Name</label>
+          <input id="email" type="email" formControlName="email">
+          <button type="submit" class="primary">Apply now</button>
+        </form>
       </section>
     </article>
   `,
@@ -36,6 +47,11 @@ export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  });
 
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params["id"]);
