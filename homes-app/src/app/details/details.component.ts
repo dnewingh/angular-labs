46dbf3e9ae56da3ad5eb5a7bb62f5bc238a1d633
Dan Newingham <dannewingham@gmail.com>
2024-03-06 10:58:11 +0000

    feat(homes-app): Add handler on form submit

--- homes-app/src/app/details/details.component.ts
+++ homes-app/src/app/details/details.component.ts
@@ -27,14 +27,14 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
       </section>
       <section class="listing-apply">
         <h2 class="section-heading">Apply to live here</h2>
-        <form [formGroup]="applyForm">
+        <form [formGroup]="applyForm" (submit)="submitApplication()">
           <label for="first-name">First Name</label>
           <input id="first-name" type="text" formControlName="firstName">
 
-          <label for="last-name">First Name</label>
+          <label for="last-name">Last Name</label>
           <input id="last-name" type="text" formControlName="lastName">
 
-          <label for="email">First Name</label>
+          <label for="email">Email</label>
           <input id="email" type="email" formControlName="email">
           <button type="submit" class="primary">Apply now</button>
         </form>
@@ -56,5 +56,12 @@ export class DetailsComponent {
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params["id"]);
     this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    )
   }
 }
