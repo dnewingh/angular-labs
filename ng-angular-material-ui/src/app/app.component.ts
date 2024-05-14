56f85a534c1a935caf0966586627be9096e493e6
Dan Newingham <dannewingham@gmail.com>
2024-05-14 20:03:20 +0000

    feat(ng-mat-ui): Add address form

--- ng-angular-material-ui/src/app/app.component.ts
+++ ng-angular-material-ui/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { AddressFormComponent } from './address-form/address-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSlideToggleModule],
+  imports: [RouterOutlet, MatSlideToggleModule, AddressFormComponent],
   template: `
     <h1>Welcome to {{title}}!</h1>
     <mat-slide-toggle>Toggle me!</mat-slide-toggle>
+    <app-address-form />
 
     <router-outlet />
   `,
